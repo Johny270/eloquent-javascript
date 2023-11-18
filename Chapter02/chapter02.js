4c98a4bf4945a24a9dfcff32e1be19c4549f68c9
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-11-18 09:13:17 -0400

    Solves Eloquent Javascript chapter 02 exercise 02

--- Chapter02/chapter02.js
+++ Chapter02/chapter02.js
@@ -1,3 +1,5 @@
+/*------------------------------------------------ EXERCISES -------------------------------------------------*/
+
 // Looping a triangle
 /*
     Write a loop that makes seven calls to console.log to output the following triangle:
@@ -16,4 +18,24 @@ console.log(item)
 while (item.length < i) {
     item += '#'
     console.log(item)
+}
+
+// FizzBuzz
+/*
+    Write a program that console.log to print all the numbers from 1 to 100,
+    with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number,
+    and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+    Bor numbers divisible by both 3 and 5 print "FizzBuzz".
+*/
+
+for(let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+        console.log('Fizz')
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+    } else {
+        console.log(i)
+    }
 }
