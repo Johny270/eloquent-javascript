c047a0b0f2f5d1915e589419d8eda66a1cb29797
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-04 17:55:43 -0400

    Solves eloquent javascript chapter 03 minimum exercise

---
+++ chapter03.js
@@ -0,0 +1,11 @@
+// Minimum
+/*
+    Write a function that takes two arguments and returns their minimum
+*/
+
+/* Solution */
+
+const minimum = function(num1, num2) {
+    if(num1 < num2) return num1;
+    return num2;
+}
