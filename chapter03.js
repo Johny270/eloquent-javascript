8f3cd24ac7f93d9e0358442caf2f0eaa1ac11772
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-04 18:11:18 -0400

    Solves eloquent javascript chapter 03 recursion exercise

--- chapter03.js
+++ chapter03.js
@@ -9,3 +9,25 @@ const minimum = function(num1, num2) {
     if(num1 < num2) return num1;
     return num2;
 }
+
+// Recursion
+/*
+    Define a recursive function 'isEven' based on these criteria:
+    - Zero is even
+    - One is odd
+    - For any other number N, its evenness is the same as N-2.
+
+    The function should accept a single parameter (a positive, whole number) and return a Boolean
+*/
+
+function isEven(num) {
+    if(num === 0) {
+        return true;
+    } else if(num === 1) {
+        return false;
+    } else if(num < 0) {
+        return isEven(-num);
+    } else {
+        return isEven(num - 2);
+    }
+}
