6a6eb90c8c8a7659298ccedc2e5ecf751984f666
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-06 17:19:21 -0400

    Solves chapter 04 'Reversing an array' exercise

--- chapter04.js
+++ chapter04.js
@@ -40,3 +40,35 @@ function sum(arr) {
     }
     return result;
 }
+
+// Reversing an array
+/*
+    Write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes
+    an array as argument and produces a new array that has the same elements in the inverse order.
+    The second, reverseArrayInPlace, does what reverse method does: it modifies the array given as
+    argument by reversing its elements. Neither may use the standard reverse method.
+*/
+
+function reverseArray(arr) {
+    let newArr = [];
+    for(let i = arr.length - 1; i >=0; i--) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
+
+function reverseArrayInPlace(arr) {
+    arr = reverseArray(arr);
+
+    // another way perhaps less efficient;
+    // count = 0;
+    // let i = 0;
+    // let j = arr.length - 1;
+    // while(count < Math.floor(arr.length / 2)) {
+    //     [arr[i], arr[j]] = [arr[j], arr[i]]
+    //     i++;
+    //     j--;
+    //     count++;
+    // }
+    return arr;
+}
